30f729ca08a5a20b3e18f6c256ed9ee5c8d47163
Danial-Hosseintabar <danial.ht10@gmail.com>
2023-02-08 19:55:44 +0330

    Camera system implemented Everything is seen through a camera. which is at some point

--- graphics.js
+++ graphics.js
@@ -1,4 +1,13 @@
 
+// dot product of two vectors in the space
+
+function dot_product( A , B ){
+    let ret = 0 ;
+    for(let i = 0 ; i < 3 ; i++) ret += A[i] * B[i] ;
+    return ret ;
+}
+
+
 // a point in the 3D - space
 
 class Point{
@@ -6,6 +15,7 @@ class Point{
         this.x = x ;
         this.y = y ;
         this.z = z ;
+        this.r = [ x , y , z ] ;
     }
 
     draw( ctx , x_0 , y_0 ){
@@ -23,6 +33,7 @@ class Point{
         this.x = x ; 
         this.y = y * Math.cos(theta) - z * Math.sin(theta) ;
         this.z = y * Math.sin(theta) + z * Math.cos(theta) ;
+        this.r = [ this.x , this.y , this.z ] ;
     }
     rotate_y(theta){
         let x = this.x ;
@@ -31,6 +42,7 @@ class Point{
         this.x = x * Math.cos(theta) + z * Math.sin(theta) ;
         this.y = y ;
         this.z = - x * Math.sin(theta) + z * Math.cos(theta) ;
+        this.r = [ this.x , this.y , this.z ] ;
     }
     rotate_z(theta){
         let x = this.x ;
@@ -39,6 +51,7 @@ class Point{
         this.x = x * Math.cos(theta) - y * Math.sin(theta);
         this.y = x * Math.sin(theta) + y * Math.cos(theta);
         this.z = z ;
+        this.r = [ this.x , this.y , this.z ] ;
     }
 };
 
@@ -50,22 +63,7 @@ class Shape{
         this.point = point ; // array of points
         this.adj = adj ; // adjacency matrix
     }
-    draw( ctx , x_0 , y_0 ){
-        for(let i = 0 ; i < this.n ; i++){
-            this.point[i].draw( ctx , x_0 , y_0 ) ;
-        }
-        for(let i = 0 ; i < this.n ; i++){
-            for(let j = i+1 ; j < this.n ; j++){
-                if( this.adj[i][j] == 1 ){
-                    ctx.beginPath() ;
-                    ctx.moveTo( this.point[i].x + x_0 , this.point[i].y + y_0 ) ;
-                    ctx.lineTo( this.point[j].x + x_0 , this.point[j].y + y_0 ) ;
-                    ctx.stroke() ;
-                    ctx.closePath() ;
-                }
-            }
-        }
-    }
+
     rotate_x(theta){
         for(let i = 0 ; i < this.n ; i++){
             this.point[i].rotate_x( theta ) ;
@@ -81,4 +79,59 @@ class Shape{
             this.point[i].rotate_z( theta ) ;
         }
     }
+};
+
+
+
+class Camera{
+    constructor( Z , X , R , theta_x , theta_y , SCREEN_SIZE ){
+        // Z and X shows whrere the camera is pointing
+        // R is where the camera actually is
+        this.theta_x = theta_x ;
+        this.theta_y = theta_y ;
+        this.SCREEN_SIZE = SCREEN_SIZE ;
+        this.R = R ;
+        this.Z = [0,0,0] ;
+        this.Y = [0,0,0] ;
+        this.X = [0,0,0] ;
+        this.Z[0] = Z[0] ;
+        this.Z[1] = Z[1] ;
+        this.Z[2] = Z[2] ;
+        this.X[0] = X[0] ;
+        this.X[1] = X[1] ;
+        this.X[2] = X[2] ;
+        this.Y[0] = this.Z[1] * this.X[2] - this.Z[2] * this.X[1] ;
+        this.Y[1] = this.Z[2] * this.X[0] - this.X[2] * this.Z[0] ; 
+        this.Y[2] = this.Z[0] * this.X[1] - this.Z[1] * this.X[0] ;
+    }
+    get_point_position( point ){
+        const dr = [0,0,0] ;
+        for(let i = 0  ; i < 3 ; i++){
+            dr[i] = point.r[i] - this.R[i] ;
+        }
+        let x_image = dot_product( dr , this.X ) / dot_product( dr , this.Z ) ;
+        let y_image = dot_product( dr , this.Y ) / dot_product( dr , this.Z ) ;
+        x_image = this.SCREEN_SIZE[0] * x_image / Math.tan(this.theta_x) ;
+        y_image = this.SCREEN_SIZE[1] * y_image / Math.tan(this.theta_y) ;
+        const ret = [ x_image , y_image ] ;
+        return ret ;
+    }
+    draw_shape( ctx , shape , x_0 , y_0 ){
+        
+        for(let i = 0 ; i < shape.n ; i++){
+            for(let j = i+1 ; j < shape.n ; j++){
+                if( shape.adj[i][j] == 1 ){
+                    ctx.beginPath() ;
+                    let pos1 = this.get_point_position(shape.point[i]) ;
+                    let pos2 = this.get_point_position(shape.point[j]) ;
+                    ctx.moveTo( x_0 + pos1[0] , y_0 + pos1[1] ) ;
+                    ctx.lineTo( x_0 + pos2[0] , y_0 + pos2[1]) ;
+                    ctx.stroke() ;
+                    ctx.closePath() ;
+                }
+            }
+        }
+        
+    }
+
 };
--- main.js
+++ main.js
@@ -2,23 +2,35 @@
 // SETTING CANVAS
 
 const mainCanvas = document.querySelector(".mainCanvas") ;
-mainCanvas.width = window.innerWidth / 2 ;
+
+/*mainCanvas.width = window.innerWidth / 2 ;
 mainCanvas.height = window.innerHeight / 2;
 let WIDTH = window.innerWidth / 2 ;
-let HEIGHT = window.innerHeight / 2 ;
+let HEIGHT = window.innerHeight / 2 ;*/
+
+mainCanvas.width = 500 ;
+mainCanvas.height = 500 ;
+let WIDTH = 500 ;
+let HEIGHT = 500 ;
 
 var ctx = mainCanvas.getContext("2d") ;
 ctx.fillStyle = "rgb(255, 255, 255)" ;
 
+const SCREEN_SIZE = [WIDTH , HEIGHT] ;
+
+// SETTING THE MAIN CAMERA
+
+var mainCamera = new Camera( [0,0,1] , [1,0,0] , [0,0,-40] , Math.PI/4 , Math.PI/4 , SCREEN_SIZE ) ;
+
 
 // SETTING CUBE
 
-let len = 50 ;
+let len = 10 ;
 const point = []
 for(let i = 0 , size = 0 ; i < 2 ; i++){
     for(let j = 0 ; j < 2 ; j++){
         for(let k = 0 ; k < 2 ; k++){
-            let p = new Point(i*len , j*len , k*len) ;
+            let p = new Point( i*len , j*len , k*len) ;
             point.push( p ) ;
         }
     }
@@ -42,28 +54,28 @@ for(let i = 0 ; i < 8 ; i++){
     }
 }
 
-
 cube = new Shape(8,point,adj) ;
 shape = cube ;
 
-shape.draw( ctx , WIDTH/2 , HEIGHT/2 ) ;
+mainCamera.draw_shape( ctx , shape , WIDTH/2 , WIDTH/2 ) ;
+
 
 function input_control( event ){
     document.getElementById("currentInput").innerText = event.key ;
     if( event.key == "x" ){
         shape.rotate_x(0.1) ;
         ctx.clearRect(0 , 0 , WIDTH , HEIGHT ) ;
-        shape.draw( ctx , WIDTH/2 , HEIGHT/2 ) ;
+        mainCamera.draw_shape( ctx , shape , WIDTH/2 , WIDTH/2  ) ;
     }
     if( event.key == "y" ){
         shape.rotate_y(0.1) ;
         ctx.clearRect(0 , 0 , WIDTH , HEIGHT ) ;
-        shape.draw( ctx , WIDTH/2 , HEIGHT/2 ) ;
+        mainCamera.draw_shape( ctx , shape , WIDTH/2 , WIDTH/2 ) ;
     }
     if( event.key == "z" ){
         shape.rotate_z(0.1) ;
         ctx.clearRect(0 , 0 , WIDTH , HEIGHT ) ;
-        shape.draw( ctx , WIDTH/2 , HEIGHT/2 ) ;
+        mainCamera.draw_shape( ctx , shape , WIDTH/2 , WIDTH/2 ) ;
     }
 }
 
