04f92a0dae2e90f19c4067f40dcfb155bccaf548
Danial-Hosseintabar <danial.ht10@gmail.com>
2023-02-14 01:19:01 +0330

    convex hull basic algorithm

--- js/graphics.js
+++ js/graphics.js
@@ -1,12 +1,17 @@
-// dot product of two vectors in the space
+
+function vec_from_to(A , B){
+    return [ -A[0]+B[0] , -A[1]+B[1] , -A[2]+B[2] ] ;
+}
+
+function cross_product( A , B ){
+    return [ A[1]*B[2] - A[2]*B[1] , A[2]*B[0] - A[0]*B[2] , A[0]*B[1]-A[1]*B[0] ] ;
+}
 
 function dot_product( A , B ){
     let ret = 0 ;
     for(let i = 0 ; i < 3 ; i++) ret += A[i] * B[i] ;
     return ret ;
 }
-
-// a point in the 3D - space
 
 class Point{
     constructor( x , y , z  ){
@@ -53,8 +58,6 @@ class Point{
     }
 };
 
-// a 3D shape in space
-
 class Shape{
     constructor( n , point , adj ){
         this.n = n ; // number of points
@@ -89,9 +92,12 @@ class Shape{
         this.n++ ;
     }
 
+    add_edge( index1 , index2 ){
+        adj[index1][index2] = 1 ;
+        adj[index2][index1] = 1 ;
+    }
+
 };
-
-
 
 class Camera{
     constructor( Z , X , R , theta_x , theta_y , SCREEN_SIZE ){
--- js/main.js
+++ js/main.js
@@ -22,45 +22,9 @@ const SCREEN_SIZE = [WIDTH , HEIGHT] ;
 
 var mainCamera = new Camera( [0,0,1] , [1,0,0] , [0,0,-40] , Math.PI/4 , Math.PI/4 , SCREEN_SIZE ) ;
 
-/*
-// SETTING CUBE
 
-let len = 10 ;
-const point = []
-for(let i = 0 , size = 0 ; i < 2 ; i++){
-    for(let j = 0 ; j < 2 ; j++){
-        for(let k = 0 ; k < 2 ; k++){
-            let p = new Point( i*len , j*len , k*len) ;
-            point.push( p ) ;
-        }
-    }
-}
-
-const adj = [] ;
-
-for(let i = 0 ; i < 8 ; i++){
-    adj.push([]) ;
-    for(let j = 0 ; j < 8 ; j++){
-        adj[i].push(0) ;
-    }
-}
-
-for(let i = 0 ; i < 8 ; i++){
-    for(let j = 0 ; j < 8 ; j++){
-        let dx = point[i].x - point[j].x ;
-        let dy = point[i].y - point[j].y ;
-        let dz = point[i].z - point[j].z ;
-        if( len*len == dx*dx + dy*dy + dz*dz ) adj[i][j] = 1 ;
-    }
-}
-
-cube = new Shape(8,point,adj) ;
-shape = cube ;
-*/
 let p = new Point(0,0,0) ;
 shape = new Shape(0 , [] , [] ) ;
-
-
 
 function update_adj( index1 , index2 ){
     shape.adj[index1][index2] = shape.adj[index1][index2] = 1 ;
@@ -82,5 +46,15 @@ function input_control( event ){
         mainCamera.draw_shape( ctx , shape , WIDTH/2 , WIDTH/2 ) ;
     }
 }
+
+
+function draw_convex_hull(){
+    let ch = new convex_hull_algo1( shape.point , shape.n ) ;
+    shape.adj = ch.solve( shape ) ;
+    mainCamera.draw_shape( ctx , shape , WIDTH/2 , WIDTH/2  ) ;
+    ch.get_vertices() ;
+    
+}
+
 
 document.addEventListener('keydown' , (event) => input_control(event) ) ;
